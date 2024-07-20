9597f37a6ab82550c83068e2bb3525443863781b
GifftyCode <ulokangozi@yahoo.com>
2024-07-20 19:54:32 +0100

    ChatWindow component logic

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -8,10 +8,51 @@ export default function ChatWindows() {
   const [loading, setLoading] = useState(false);
   const [input, setInput] = useState('');
 
+  const sendMessage = async (userInput) => {
+    // add user message to state
+    const userMessage = { sender: 'user', text: userInput };
+    setMessages([...messages, userMessage]);
+
+    // set loading to true
+    setLoading(true);
+
+    try {
+      console.log('API call goes here...');
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (input.trim() !== '') {
+      sendMessage(input);
+      setInput('');
+    }
+  };
+
   return (
     <div>
-      <Message />
-      <LoadingIndicator />
+      {/* Display all messages */}
+      {messages.map((message, index) => (
+        <Message key={index} sender={message.sender} text={message.text} />
+      ))}
+
+      {/* Display Loading Indicator */}
+      {loading && <LoadingIndicator />}
+
+      {/* Display Input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   );
 }
